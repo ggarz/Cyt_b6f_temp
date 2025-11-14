e0e6830de766ff159534d9106a08f8c33467c580
Garrison Garza <55ggarz@gmail.com>
2025-11-14 14:00:59 -0600

    added licor data

--- garry_test.R
+++ garry_test.R
@@ -5,10 +5,13 @@ library(ggthemes)
 
 source('garry_model.R')
 
-test_1 <- model(PAR =  seq(0, 1500, 100))
+test_1 <- model(PAR =  seq(0, 2000, 200))
 
 test_2 <- model(PAR =  seq(0, 1500, 100), CO2 = 800)
 
-test_3 <- model(PAR =  seq(0, 1500, 100), temp_c = 35)
+test_3 <- model(PAR =  seq(0, 2000, 200), temp_c = 35)
 
 test_4 <- model(temp_c = seq(0, 50, 5))
+
+
+
